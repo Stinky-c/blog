e81d3a6607286d2905564daaf8276de233cf68e4
Bucky <60587749+Stinky-c@users.noreply.github.com>
2025-04-08 23:58:15 +0000

    Fighting with wrangler

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,6 +4,9 @@ export default defineNuxtConfig({
     devtools: { enabled: true },
     nitro: {
         preset: "cloudflare_pages",
+        prerender: {
+            autoSubfolderIndex: false,
+        },
     },
     content: {
         build: {
