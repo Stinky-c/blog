3a30651db88534eca21329e3be75ebc739ddf6ec
Bucky <60587749+Stinky-c@users.noreply.github.com>
2025-05-08 00:50:04 +0000

    Small contactme changes

--- pages/index.vue
+++ pages/index.vue
@@ -6,14 +6,18 @@
         <div class="max-w-md">
           <h1 class="text-5xl font-bold">Hello!</h1>
           <p class="py-6">
-            This is a personal site created to home blog articles and
-            collections of documents relating to various topics. See the
-            nagivation bar above to see resources. Powered by
+            This is a work in progress personal site created to home blog articles and
+            collections of documents relating to various topics and projects I work on.
+            See the navigation bar above to see resources. Powered by
             <NuxtLink to="https://nuxt.com/" class="link">Nuxt</NuxtLink>,
             <NuxtLink to="https://daisyui.com/" class="link">daisyUI</NuxtLink>,
             and
             <NuxtLink to="https://pages.cloudflare.com/" class="link"
               >Cloudflare Pages</NuxtLink
+            >.<br />
+            Please contact me at
+            <NuxtLink to="mailto:bucky@buckydev.com" class="link"
+              >bucky@buckydev.com</NuxtLink
             >.
           </p>
         </div>
